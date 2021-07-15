ef1bc316e278cd3f933935abce41169e89d275a7
FULLSERVICE\flavio marsili <f.marsili@full.srl>
2021-07-15 13:27:56 +0200

    controllers webapi

---
+++ WebApi-Controllers/CheckoutController.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Stripe;
+using Stripe.Checkout;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace WebApi.Controllers
+{
+    public class CheckoutController : ApiController
+    {
+        [HttpPost]
+        public async Task<IHttpActionResult> Checkout(DTO.Basket basket)
+        {
+            return await Task.Run<IHttpActionResult>(() =>
+            {
+                SessionCreateOptions sessionCreateOptions = new SessionCreateOptions
+                {
+                    SuccessUrl = "https://redirect_uri/stato/1",
+                    CancelUrl = "https://redirect_uri/stato/0",
+                    PaymentMethodTypes = new List<string>
+                {
+                    "card"
+                },
+                    LineItems = new List<SessionLineItemOptions>(),
+                    Metadata = new Dictionary<string, string>()
+                {
+                    { "value1", "value1" },
+                    { "value2", "value2" }
+                },
+                    Mode = "payment"
+                };
+				foreach(DTO.Item item in basket)
+					sessionCreateOptions.LineItems.Add(new SessionLineItemOptions()
+					{
+						PriceData = new SessionLineItemPriceDataOptions()
+						{
+							Currency = "eur",
+							UnitAmount = item.price * 100 //CENT,
+							ProductData = new SessionLineItemPriceDataProductDataOptions()
+							{
+								Name = item.name
+							}
+						},
+						Quantity = item.quantity
+					});
+                return Ok(Guid.NewGuid().ToString())
+            });
+        }
+    }
+}
---
+++ WebApi-Controllers/WebHookController.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/webhook")]
+    public class WebHookController : ApiController
+    {
+        [HttpPost]
+        [Route("checkout")]
+        public async Task<IHttpActionResult> Post()
+        {
+            var json = await this.ActionContext.Request.Content.ReadAsStringAsync();
+            try
+            {
+                var stripeEvent = EventUtility.ConstructEvent(json,
+                this.ActionContext.Request.Headers.GetValues("Stripe-Signature").ElementAt(0),
+					ConfigurationManager.AppSettings["wh_session_successed"]);
+                if (stripeEvent.Type == Events.CheckoutSessionCompleted)
+                {
+                    var checkoutSession = stripeEvent.Data.Object as Stripe.Checkout.Session;
+                    //TODO CUSTOM CODE WITH METADATA
+					//checkoutSession.Metadata["value1"]
+					//checkoutSession.Metadata["value2"]
+                    //READ CHECKOUT FOR ITEMS
+                }
+                return Ok();
+            }
+            catch (StripeException ex)
+            {
+                return BadRequest();
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
